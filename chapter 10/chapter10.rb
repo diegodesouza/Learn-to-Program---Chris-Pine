ab8b85e41a49b61131b48d6dca496ff5562bac7a
Diego De Souza <diegotrus7@gmail.com>
2015-03-10 11:56:33 -0400

    count won't go off the edge

--- chapter 10/chapter10.rb
+++ chapter 10/chapter10.rb
@@ -43,19 +43,24 @@
 M = 'land'
 o = 'water'
 
-world = [[o,o,o,o,o,o,o,o,o,o,o],
+world = [[o,o,o,o,o,M,o,o,o,o,o],
          [o,o,o,o,M,M,o,o,o,o,o],
-         [o,o,o,o,o,o,o,o,M,M,o],
-         [o,o,o,M,o,o,o,o,o,M,o],
-         [o,o,o,M,o,M,M,o,o,o,o],
+         [o,o,o,o,o,M,o,o,M,M,o],
+         [o,o,o,M,o,M,o,o,o,M,o],
+         [o,o,o,o,o,M,M,o,o,o,o],
          [o,o,o,o,M,M,M,M,o,o,o],
-         [o,o,o,M,M,M,M,M,M,M,o],
+         [M,M,M,M,M,M,M,M,M,M,M],
          [o,o,o,M,M,o,M,M,M,o,o],
          [o,o,o,o,o,o,M,M,o,o,o],
-         [o,M,o,o,o,M,o,o,o,o,o],
+         [o,M,o,o,o,M,M,o,o,o,o],
+         [o,o,o,o,o,M,o,o,o,o,o],
          [o,o,o,o,o,o,o,o,o,o,o]]
 
 def continent_size world, x, y
+  if x < 0 || x > 10 || y < 0 || y > 10
+    return 0
+  end
+
   if world[y][x] != 'land'
     # Either it's water or we already
     # counted it. but either way, we don't
